85fbbea4b9ec76307406fa37d63c1cd3a94523d9
imavazq <ruterima@gmail.com>
2025-02-14 12:28:44 -0300

    ADDED: Configuracion inicial docker-compose y microservicio Config server

---
+++ services/config-server/src/test/java/com/imavazq/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imavazq.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
